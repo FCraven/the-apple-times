defe2721d4868643895c973e080c6ccddefbb448
FCraven <craven.francis@gmail.com>
2022-03-16 22:40:57 -0400

    lightened mobile menu button colors made background transparent added backdrop blur

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -29,7 +29,7 @@ const mobileMenuButtons = [
 const MobileMenu =()=> {
 
   return (
-    <ul className='h-full w-full flex justify-around items-center'>
+    <ul className='h-full w-full flex justify-around items-center bg-transparent backdrop-blur-md'>
 
       { mobileMenuButtons.map((el,index)=> <MobileMenuButton key={index} text={el.text} icon={el.icon} /> )}
 
--- src/components/MobileMenuButton/MobileMenuButton.js
+++ src/components/MobileMenuButton/MobileMenuButton.js
@@ -3,8 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const MobileMenuButton =({icon, text})=> {
   return (
     <li className='flex flex-col justify-center items-center h-full w-auto'>
-      <FontAwesomeIcon className='p-1' icon={icon} size='2x' />
-      <p className='text-lg'>{text}</p>
+      <FontAwesomeIcon className='p-1 text-slate-400' icon={icon} size='2x' />
+      <p className='text-lg text-slate-400'>{text}</p>
     </li>
   )
 }
