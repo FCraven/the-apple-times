f011b3c34e83430d1eeff87a58d129232d4e9f60
FCraven <craven.francis@gmail.com>
2022-03-22 23:28:05 -0400

    parsing date for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAppleWhole } from '@fortawesome/free-solid-svg-icons'
 
 const Header =()=> {
-  const date = new Date();
+  const date = new Date().toString();
+  const splitDate = date.split(' ');
+
+  const months = {
+    Jan : 'January',
+    Feb: 'February',
+    Mar: 'March',
+    Apr: 'April',
+    May: 'May',
+    Jun: 'June',
+    Jul: 'July',
+    Aug: 'August',
+    Sep: 'September',
+    Oct: 'October',
+    Nov: 'November',
+    Dec: 'December'
+  }
+
+  console.log('Date', date)
+
+
+
+
 
   return (
     <header className='border border-b-slate-700 h-16 w-full relative'>
       <FontAwesomeIcon icon={ faAppleWhole } />
-      <time>{`${date.getMonth()} ${date.getDay()}`}</time>
+      <time>{`${months[splitDate[1]]} ${splitDate[2]}`}</time>
     </header>
   )
 }
