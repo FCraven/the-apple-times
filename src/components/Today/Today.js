911bac81a67724b2832ab4368d3405e4c2943eff
FCraven <craven.francis@gmail.com>
2022-03-29 22:14:10 -0400

    main headline style MVP done, need click handlers for buttons and card

--- src/components/Today/Today.js
+++ src/components/Today/Today.js
@@ -1,4 +1,7 @@
 import { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'
 
 const Today =()=> {
@@ -20,35 +23,39 @@ const Today =()=> {
   }, [])
 
   const mainHeadline = articles[0];
+
+  console.log(mainHeadline)
   return (
     <section className='block'>
       <h2 className='font-black bold text-3xl text-red-500 py-2'>Top Stories</h2>
       <ul>
         {/* headline */}
-        <li className='w-full flex flex-col bg-white rounded-xl border border-black'>
-          <img className='rounded-t-xl' src={articles[0]?.multimedia[1]?.url} alt="" />
+        <li className='w-full flex flex-col bg-white dark:bg-slate-800 t rounded-xl '>
+          <img className='rounded-t-xl' src={mainHeadline?.multimedia[1]?.url} alt="" />
           <hgroup className='p-4'>
-            <p className='text-medium font-bold text-blue-600'>
+            <p className='text-medium font-extrabold text-blue-600 dark:text-yellow-600'>
               {mainHeadline?.section.toUpperCase()}
             </p>
-            <h1 className='text-3xl font-extrabold py-1'>
+            <h1 className='text-3xl dark:text-slate-50 font-extrabold py-1'>
               {mainHeadline?.title}
             </h1>
-            <h3 className='py-2 text-lg text-slate-500 font-light'>
+            <h3 className='py-2 text-lg text-slate-500 dark:text-slate-100 font-light'>
               {mainHeadline?.abstract}
             </h3>
           </hgroup>
+          <nav className='h-10 border-t flex items-center justify-between rounded-b-lg'>
+            <NavLink to={'/'} className='flex items-center justify-center rounded-full bg-slate-500 px-4 mx-3 text-white text-md font-black'>{`More coverage >`}</NavLink>
+            <FontAwesomeIcon icon={faEllipsis} size='xl' className='text-white px-4'/>
+          </nav>
+
         </li>
         { articles.map((el,index)=> {
           return (
-            <li key={index} className='flex border-b border-red-500 my-2'>
-              <img src={el.multimedia[2].url} alt="" className='rounded-l-lg' />
+            <li key={index} className='flex items-center border-b border-slate-500 my-2 '>
               <div className='px-2 pb-2'>
-                <hgroup>
                   <h3 className='text-lg font-black dark:text-red-100'>{el.title}</h3>
-                  <h5 className='text-md font-medium dark:text-red-100'>{el.abstract}</h5>
-                </hgroup>
               </div>
+              <img src={el.multimedia? el.multimedia[2].url : ''} alt="" className='rounded-lg h-24 w-24 ' />
             </li>
           )
         }).slice(1)}
