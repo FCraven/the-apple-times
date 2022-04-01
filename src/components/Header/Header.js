c30532c5b79edbc2f0214775da15a43fa9634926
FCraven <craven.francis@gmail.com>
2022-04-01 13:32:08 -0400

    created helper to remove leading 0 from date

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAppleWhole } from '@fortawesome/free-solid-svg-icons'
 
 const Header =()=> {
+
   const date = new Date().toString().split(' ');
 
   const months = {
@@ -19,8 +20,13 @@ const Header =()=> {
     Dec: 'December'
   }
 
-  const month = months[date[1]]
-  const day = date[2]
+  const month = months[date[1]];
+  const day = date[2];
+
+  const dayMaker =(str)=> {
+    const splitStr = str.split('');
+    return splitStr[0] === '0' ? splitStr[1] : str;
+  }
 
   return (
     <header className='flex flex-col border-b border-slate-300 relative top-0 mb-8 pb-1 dark:bg-black'>
@@ -28,7 +34,9 @@ const Header =()=> {
         <FontAwesomeIcon icon={ faAppleWhole } className='text-black dark:text-white mr-1 text-3xl'/>
         <h2 className='flex text-4xl font-black dark:text-white'>Times</h2>
       </div>
-      <time className='flex items-start text-slate-300 dark:text-slate-500 text-4xl mb-2 font-black leading-7'>{`${month} ${day}`}</time>
+      <time className='flex items-start text-slate-300 dark:text-slate-500 text-4xl mb-2 font-black leading-7'>
+        {`${month} ${dayMaker(day)}`}
+      </time>
     </header>
   )
 }
