8d6d71018e7ccf071c95e40d186c9df8062140c1
FCraven <craven.francis@gmail.com>
2022-04-06 13:42:08 -0400

    MoreCoverageMenu styled now for handlers

--- src/components/Today/Today.js
+++ src/components/Today/Today.js
@@ -6,7 +6,9 @@ import axios from 'axios'
 
 const Today =()=> {
 
-  const [ articles, setArticles ] = useState([])
+  const [ articles, setArticles ] = useState([]);
+  const [ isCoverageMenuOpen, setCoverageMenu ] = useState(false);
+  const [ isEllipsisMenuOpen, setEllipsisMenu ] = useState(false);
 
   useEffect(()=> {
     const getTodaysHeadlines = async() => {
@@ -26,43 +28,78 @@ const Today =()=> {
 
   console.log(mainHeadline)
 
-
-
   return (
     <section className='block'>
       <h2 className='font-black bold text-3xl text-red-500 py-2'>Top Stories</h2>
       <ul>
-        {/* headline */}
-        <li className='w-full flex flex-col bg-white dark:bg-slate-800 t rounded-xl'>
 
-          <div onClick={()=> console.log('clicked article')}>
+        {/* HEADLINE COMPONENT*/}
+        <li className='w-full flex flex-col bg-white dark:bg-slate-800 rounded-xl '>
+
+          {/* CARD MAIN */}
+
+          <article onClick={()=> console.log('clicked article')}>
+
             <img className='rounded-t-xl' src={mainHeadline?.multimedia[1]?.url} alt="" />
+
+            {/* BODY */}
+
             <hgroup className='p-4'>
+
+              {/* SECTION NAME */}
               <p className='text-medium font-extrabold text-blue-600 dark:text-yellow-600'>
                 {mainHeadline?.section.toUpperCase()}
               </p>
+              {/* HEADLINE */}
               <h1 className='text-3xl dark:text-slate-50 font-extrabold py-1'>
                 {mainHeadline?.title}
               </h1>
+              {/* SUBHEADING */}
               <h3 className='py-2 text-lg text-slate-500 dark:text-slate-100 font-light'>
                 {mainHeadline?.abstract}
               </h3>
+
             </hgroup>
-          </div>
+
+          </article>
+
+          {/* Bottom Bar  */}
 
           <nav className='h-10 border-t flex items-center justify-between rounded-b-lg'>
+
+            {/* MORE COVERAGE BUTTON  */}
             <NavLink  to={`/`}
-                      onClick={()=> console.log(mainHeadline.des_facet)}
-                      className='flex items-center justify-center rounded-full bg-slate-500 px-4 mx-3 text-white text-md font-black'>
-              {`More coverage >`}
+                      onClick={()=> setCoverageMenu(!isCoverageMenuOpen)}
+                      className={`relative flex items-center justify-center ${isCoverageMenuOpen? 'rounded-b-lg bg-slate-500/70': 'rounded-full'} bg-slate-500 px-4 mx-3 text-white text-md font-black`}>
+              {isCoverageMenuOpen? 'CLOSE': `More coverage >` }
+
+              {/* MENU CONTAINER */}
+              {
+                isCoverageMenuOpen &&
+                  <ul className='absolute flex flex-col place-content-around left-0 bottom-6 h-auto w-96 bg-slate-500/70 backdrop-blur-sm pt-6 px-6 py-2 rounded-t-lg rounded-br-lg '>
+
+                    { mainHeadline?.des_facet.map((el, index)=> {
+                      return <li key={index} className='w-full flex justify-center items-center h-12 mb-2 rounded-lg bg-slate-500/300 backdrop-blur-md hover:border-b-1 hover:border-white'>{el}</li>
+                    })}
+
+                  </ul>
+              }
             </NavLink>
+
+
             <FontAwesomeIcon  icon={faEllipsis}
                               size='xl'
                               className='dark:text-white px-4'
-                              onClick={()=> console.log('clicked ellipsis')}/>
+                              onClick={()=> setEllipsisMenu(!isEllipsisMenuOpen)}/>
           </nav>
 
         </li>
+        {/* END MAIN HEADLINE */}
+
+        {/* TODO: SIDE BY SIDE */}
+
+        {/* REST OF TOP HEADLINES */}
+
         {
           articles.slice(1).map((el,index)=> {
             return (
